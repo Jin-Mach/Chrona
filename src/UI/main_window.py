f65a90be3d5a244ab4c6ea5d06605955a8cfa6fe
Jin-Mach <ji82ma@seznam.cz>
2026-02-06 17:12:50 +0100

    Create SettingsWidget UI with folder, file type, name, and delete options. Setup initial states and connections.

--- src/UI/main_window.py
+++ src/UI/main_window.py
@@ -1,12 +1,22 @@
 from PyQt6.QtGui import QShowEvent
-from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout
+
+from src.UI.widgets.settings_widget import SettingsWidget
 
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
-        self.setObjectName("mainWindow")
         self.centered = False
+        self.settings_widget = SettingsWidget(self)
+        self.setCentralWidget(self.create_gui())
+
+    def create_gui(self) -> QWidget:
+        central_widget = QWidget()
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.settings_widget)
+        central_widget.setLayout(main_layout)
+        return central_widget
 
     def showEvent(self, event: QShowEvent) -> None:
         super().showEvent(event)
