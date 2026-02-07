0bcbd3529e9a4ebd6c6b6791c4714ff353ca6f54
Jin-Mach <ji82ma@seznam.cz>
2026-02-07 20:03:42 +0100

    In LanguageProvider create get_text_data method return dictionary with UI texts. ConfigProvider now checks that settings.toml exists and is a file before loading.

--- src/providers/config_provider.py
+++ src/providers/config_provider.py
@@ -13,9 +13,10 @@ class ConfigProvider:
     def get_config_data(class_name: str | None = None) -> dict[str, Any]:
         config_data = {}
         try:
-            if not ConfigProvider.project_path.exists():
-                raise FileNotFoundError(f"Config file {ConfigProvider.project_path} not found")
-            with open(ConfigProvider.project_path.joinpath("config", "settings.toml"), "rb") as config_file:
+            config_path = ConfigProvider.project_path.joinpath("config", "settings.toml")
+            if not config_path.exists() or not config_path.is_file():
+                raise FileNotFoundError(f"Config file {config_path} loading failed")
+            with open(config_path, "rb") as config_file:
                 config_data = tomllib.load(config_file)
             if class_name is not None:
                 config_data = config_data.get(class_name, {})
--- src/providers/language_provider.py
+++ src/providers/language_provider.py
@@ -1,8 +1,11 @@
+import json
+
 from typing import Any
 
 from PyQt6.QtCore import QLocale
 
 from src.providers.config_provider import ConfigProvider
+from src.utilities.current_path_provider import set_project_path
 from src.utilities.error_handler import Errorhandler
 
 
@@ -14,6 +17,7 @@ class LanguageProvider:
 
     @classmethod
     def basic_setup(cls) -> None:
+        cls.project_path = set_project_path()
         cls.name = cls.__name__
         cls.config_data = ConfigProvider.get_config_data(class_name=cls.name)
         cls.language_code = cls.config_data.get("default_language", "en_GB")
@@ -28,3 +32,18 @@ class LanguageProvider:
         except Exception as e:
             Errorhandler.handle_error(cls.name, e)
         return cls.language_code
+
+    @classmethod
+    def get_texts_data(cls, file_name: str, widget_name: str, language_code: str | None = None) -> dict[str, str]:
+        texts_data = {}
+        try:
+            json_file = file_name + ".json"
+            if language_code is None:
+                language_code = cls.language_code
+            texts_path = cls.project_path.joinpath("resources", "texts", language_code, json_file)
+            if texts_path.exists() and texts_path.is_file():
+                with open(texts_path, "r", encoding="utf-8") as texts_file:
+                    texts_data = json.load(texts_file).get(widget_name, {})
+        except Exception as e:
+            Errorhandler.handle_error(cls.name, e)
+        return texts_data
