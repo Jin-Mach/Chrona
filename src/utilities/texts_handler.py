81a07912cf1ca7da0b1f671be25d12ef89e6d989
Jin-Mach <ji82ma@seznam.cz>
2026-02-08 16:33:15 +0100

    Create texts_handler.py file with set_ui_texts method for setup UI texts from json files data.

---
+++ src/utilities/texts_handler.py
@@ -0,0 +1,25 @@
+from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QMainWindow, QGroupBox, QCheckBox, QRadioButton
+
+from src.utilities.error_handler import Errorhandler
+
+default_text = "N/A"
+
+
+def set_ui_texts(top_widget: QWidget, texts_data: dict[str, str], widgets: list[QWidget]) -> None:
+    try:
+        if "titleText" in texts_data:
+            if isinstance(top_widget, QMainWindow):
+                top_widget.setWindowTitle(texts_data["titleText"])
+        for widget in widgets:
+            if bool(widget.objectName()):
+                key_name = widget.objectName() + "Text"
+                if key_name in texts_data:
+                    text = texts_data[key_name]
+                    if isinstance(widget, (QLabel, QPushButton, QCheckBox, QRadioButton)):
+                        widget.setText(text)
+                    elif isinstance(widget, QLineEdit):
+                        widget.setPlaceholderText(text)
+                    elif isinstance(widget, QGroupBox):
+                        widget.setTitle(text)
+    except Exception as e:
+        Errorhandler.handle_error("UITextsHandler", e)
